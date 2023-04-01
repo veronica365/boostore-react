c06688dbb17e57080b5a7a7dcd3f75b1e4f748d5
veronica365 <naruth10@gmail.com>
2023-04-01 13:24:58 +0300

    Add routes to the App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Route, BrowserRouter, Routes, Navigate,
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import BookList from './components/Book/BookList';
+import Category from './components/Categories/Category';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <div className="wrapper">
+        <Routes>
+          <Route path="/" element={<BookList />} />
+          <Route path="/categories" element={<Category />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
