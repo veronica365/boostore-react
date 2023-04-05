9248df37472e47c701d721546955f2ccf88cf73b
veronica365 <naruth10@gmail.com>
2023-04-05 09:36:59 +0300

    Display books when the components renders

--- src/components/Book/BookList/index.js
+++ src/components/Book/BookList/index.js
@@ -1,9 +1,16 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import BookItem from '../BookItem';
 import NewBook from '../NewBook';
+import { getBooks } from '../../../redux/books/booksSlice';
 
 export default function BookList() {
   const books = useSelector((store) => store.books.books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
+
   return (
     <div>
       {books.map((book) => (
