02ce98c7c482297466c3d2a894cc1886f5ca548c
veronica365 <naruth10@gmail.com>
2023-04-05 10:01:33 +0300

    Add button components to remove and add a book

--- src/components/Book/BookItem/index.js
+++ src/components/Book/BookItem/index.js
@@ -1,20 +1,13 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { removeBook } from '../../../redux/books/booksSlice';
+import BookRemove from '../BookRemoveButton';
 
 export default function Book({ book }) {
-  const dispatch = useDispatch();
-  const handleRemoveBook = () => {
-    dispatch(removeBook(book.item_id));
-  };
   return (
     <div>
       <h3>{book.title}</h3>
       <p>{book.author}</p>
-      <button type="button" onClick={handleRemoveBook}>
-        Remove
-      </button>
+      <BookRemove id={book.item_id} />
     </div>
   );
 }
