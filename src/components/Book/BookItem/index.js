d072fc7dcf841528c825397052f33d3a4c559737
veronica365 <naruth10@gmail.com>
2023-04-05 07:31:44 +0300

    Add functionality to remove a book

--- src/components/Book/BookItem/index.js
+++ src/components/Book/BookItem/index.js
@@ -1,18 +1,27 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../../../redux/books/booksSlice';
 
 export default function Book({ book }) {
+  const dispatch = useDispatch();
+  const handleRemoveBook = () => {
+    dispatch(removeBook(book.item_id));
+  };
   return (
     <div>
       <h3>{book.title}</h3>
       <p>{book.author}</p>
-      <button type="button">Remove</button>
+      <button type="button" onClick={handleRemoveBook}>
+        Remove
+      </button>
     </div>
   );
 }
 
 Book.propTypes = {
   book: PropTypes.shape({
+    item_id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
   }).isRequired,
