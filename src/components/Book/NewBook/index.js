5b9da9cff56fb03465d3fdc10e6db49faf2c250e
veronica365 <naruth10@gmail.com>
2023-04-05 08:25:48 +0300

    Add functionality to add and remove a book

--- src/components/Book/NewBook/index.js
+++ src/components/Book/NewBook/index.js
@@ -1,12 +1,25 @@
 import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addBook } from '../../../redux/books/booksSlice';
 
 export default function NewBook() {
   const [state, setState] = useState({});
+  const dispatch = useDispatch();
+  let lastBookId = useSelector((store) => store.books.books);
+  lastBookId = lastBookId[lastBookId.length - 1]?.item_id || 'item0';
+  lastBookId = lastBookId.replace('item', '');
+  lastBookId = lastBookId && parseInt(lastBookId, 10);
+  lastBookId = `item${lastBookId + 1}`;
   const handleChange = (e) => {
-    setState({ [e.target.name]: e.target.value });
+    setState((prev) => ({ ...prev, [e.target.name]: e.target.value.trim() }));
   };
   const onSubmit = (e) => {
     e.preventDefault();
+    if (!state.title?.trim() || !state.author?.trim()) return;
+    const newBook = { ...state, category: 'Fiction', item_id: lastBookId };
+    dispatch(addBook(newBook));
+    e.target.elements.title.value = '';
+    e.target.elements.author.value = '';
   };
   return (
     <div>
