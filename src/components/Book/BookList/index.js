a771292a4d60345a476f9d3d41f620baf83560a1
veronica365 <naruth10@gmail.com>
2023-04-05 07:31:44 +0300

    Add functionality to remove a book

--- src/components/Book/BookList/index.js
+++ src/components/Book/BookList/index.js
@@ -1,34 +1,13 @@
+import { useSelector } from 'react-redux';
 import BookItem from '../BookItem';
 import NewBook from '../NewBook';
 
 export default function BookList() {
-  const books = [
-    {
-      id: 1,
-      genre: 'Action',
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-      completed: '64%',
-    },
-    {
-      id: 2,
-      genre: 'Science Fiction',
-      title: 'Dune',
-      author: 'Frank Herbert',
-      completed: '6%',
-    },
-    {
-      id: 3,
-      genre: 'Economy',
-      title: 'Capital in the Twenty-First Century',
-      author: 'Suzanne Collins',
-      completed: '0%',
-    },
-  ];
+  const books = useSelector((store) => store.books.books);
   return (
     <div>
       {books.map((book) => (
-        <BookItem key={book.id} book={book} />
+        <BookItem key={book.item_id} book={book} />
       ))}
       <NewBook />
     </div>
