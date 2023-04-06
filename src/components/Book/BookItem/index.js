ac5aaba7c0fa346bb335941a0b17c654d5c5322c
veronica365 <naruth10@gmail.com>
2023-04-06 05:26:20 +0300

    Add styles for the book item and navbar

--- src/components/Book/BookItem/index.js
+++ src/components/Book/BookItem/index.js
@@ -4,10 +4,19 @@ import BookRemove from '../BookRemoveButton';
 
 export default function Book({ book }) {
   return (
-    <div>
-      <h3>{book.title}</h3>
-      <p>{book.author}</p>
-      <BookRemove id={book.itemId} />
+    <div className="book-item">
+      <div className="book-info">
+        <span className="book-category">{book.category}</span>
+        <h3 className="book-title">{book.title}</h3>
+        <p>{book.author}</p>
+        <div className="book-actions">
+          <button type="button">Comments</button>
+          <div className="divider" />
+          <BookRemove id={book.itemId} />
+          <div className="divider" />
+          <button type="button">Edit</button>
+        </div>
+      </div>
     </div>
   );
 }
@@ -17,5 +26,6 @@ Book.propTypes = {
     itemId: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
   }).isRequired,
 };
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,20 +3,21 @@ import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div className="nav-bar">
-      <nav className="nav container-width">
-        <div>
-          <h1>Bookstore CMS</h1>
-        </div>
-        <ul className="links">
-          <li>
-            <NavLink to="/">Books</NavLink>
-          </li>
-          <li>
-            <NavLink to="/categories">Categories</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className="nav-bar">
+      <div className="logo">
+        <h1>Bookstore CMS</h1>
+      </div>
+      <ul className="links">
+        <li>
+          <NavLink to="/">Books</NavLink>
+        </li>
+        <li>
+          <NavLink to="/categories">Categories</NavLink>
+        </li>
+      </ul>
+      <button className="icon-button" type="button">
+        <span className="material-icons">person</span>
+      </button>
+    </nav>
   );
 }
